12926cae2f1f61085f700e0a2d9b7c4fddbb1329
Damilola-Nuga <damiloladaniel357@gmail.com>
2023-11-14 16:33:34 -0800

    main.h

--- main.h
+++ main.h
@@ -7,6 +7,21 @@ int printf_string(va_list args);
 <<<<<<< HEAD
 int printf_i(va_list args);
 int printf_d(va_list args);
+int print_rot13(va_list list);
+int print_String(va_list l)
+int isNonAlphaNumeric(char c);
+char *convert(unsigned long int num, int base, int lowercase);
+int printf_HEX(va_list val);
+int printf_bin(va_list val);
+int print_hex_aux(unsigned long int num);
+int printf_oct(va_list val);
+int print_pointer(va_list val);
+int print_rev(va_list l);
+int _strlen(char *s);
+int _strlenc(const char *s);
+int printf_unsigned(va_list args);
+int printf_hex(va_list val);
+
 =======
 int print_37(void);
 
